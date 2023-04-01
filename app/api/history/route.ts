db994461b7f15eff41b3a35fc1cb97da2bbcebee
Denis Carriere <carriere.denis@gmail.com>
2023-04-01 14:21:56 -0400

    make history serverless

--- app/api/history/route.ts
+++ app/api/history/route.ts
@@ -5,11 +5,12 @@ export interface History {
 }
 
 export function GET(request: Request ) {
+    const searchParams = new URL(request.url).searchParams;
     const data: History[] = [
         {address: "0xed33259a056f4fb449ffb7b7e2ecb43a9b5685bf", timestamp: new Date().getTime() - 10000},
-        {address: "0xebec795c9c8bbd61ffc14a6662944748f299cacf", timestamp: new Date().getTime() - 30000},
-        {address: "0x9b1f7f645351af3631a656421ed2e40f2802e6c0", timestamp: new Date().getTime() - 60000},
-        {address: "0x388c818ca8b9251b393131c08a736a67ccb19297", timestamp: new Date().getTime() - 100000},
+        {address: "0xebec795c9c8bbd61ffc14a6662944748f299cacf", timestamp: new Date().getTime() - 50000},
+        {address: "0x9b1f7f645351af3631a656421ed2e40f2802e6c0", timestamp: new Date().getTime() - 100000},
+        {address: "0x388c818ca8b9251b393131c08a736a67ccb19297", timestamp: new Date().getTime() - 200000},
     ]
     return toJSON(data);
 }
