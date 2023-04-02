27a2220a3ce4612623688d6bd73e08c86dd1c2a7
Denis Carriere <carriere.denis@gmail.com>
2023-04-02 09:18:42 -0400

    add length to sanitze

--- src/lib/sanitizeAddress.ts
+++ src/lib/sanitizeAddress.ts
@@ -1,4 +1,5 @@
 export const sanitizeAddress = (address: string) => {
+  const len = 7
   if (address.length <= 12) return address;
-  return address.slice(0, 4) + "..." + address.slice(-4);
+  return address.slice(0, len) + "..." + address.slice(-len);
 };
