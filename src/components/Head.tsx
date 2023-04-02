60976ee5c344f2b5b50a544042154bffd17fc0a3
Denis Carriere <carriere.denis@gmail.com>
2023-04-02 09:45:20 -0400

    add Google site verification meta

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -18,6 +18,7 @@ export const Head = () => {
         name="keywords"
         content="EOS, EVM, faucet, testnet, balance, free, EOS"
       />
+      <meta name="google-site-verification" content="IiJqa5EsAcb2Go6AzN_FrKNPxlousZXI0YKPKCQ8CAw" />
       <link rel="icon" href="/icon.png" />
     </_Head>
   );
