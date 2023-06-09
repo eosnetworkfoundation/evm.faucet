7ed80d72de87e17300a11afee6f4a3edf5042915
juanm95 <juanmarroquin95@gmail.com>
2023-06-09 16:44:17 -0700

    WIP

---
+++ src/components/Navbar.svelte
@@ -0,0 +1,14 @@
+<script>
+
+</script>
+
+<nav class="fixed w-full bg-transparent ">
+    <div className="nav-background"></div>
+    <div className="nav-content contained">
+      <div class="flex">
+        <a class="center" href="https://explorer.testnet.evm.eosnetwork.com" target="_blank" rel="noreferrer">
+            <img width={100} height={100} src="/eos-evm.svg" alt="EOS EVM" />
+        </a>
+      </div>
+    </div>
+  </nav>
