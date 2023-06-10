a8224c4530bfeb53db0ff55ebb14c5e40997eb97
juanm95 <juanmarroquin95@gmail.com>
2023-06-09 18:03:13 -0700

    Navbar and background

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -1,14 +1,15 @@
-<script>
-
-</script>
-
-<nav class="fixed w-full bg-transparent ">
-    <div className="nav-background"></div>
-    <div className="nav-content contained">
-      <div class="flex">
-        <a class="center" href="https://explorer.testnet.evm.eosnetwork.com" target="_blank" rel="noreferrer">
-            <img width={100} height={100} src="/eos-evm.svg" alt="EOS EVM" />
-        </a>
-      </div>
+<nav class="fixed w-full bg-transparent text-white z-10">
+    <div class="bg-black bg-opacity-0 blur-0 backdrop-blur-0" />
+    <div class="flex justify-between items-center py-4 px-4 contained">
+        <div class="flex">
+            <a
+                class="center"
+                href="https://explorer.testnet.evm.eosnetwork.com"
+                target="_blank"
+                rel="noreferrer"
+            >
+                <img width={100} height={100} src="/eos-evm.svg" alt="EOS EVM" />
+            </a>
+        </div>
     </div>
-  </nav>
+</nav>
